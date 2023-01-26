8fb8af8d2fbfcd5a40e0c21cd6954b988ab334a6
Charanreddy-24 <97534262+Charanreddy-24@users.noreply.github.com>
2023-01-26 20:38:44 +0530

    Add files via upload

---
+++ debug2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdio.h>
+int main ()
+{
+    char c[],c1[];
+    int t=0,i,j;
+    gets(c);
+    for (i=0,j=0;c[i]!='\0';i++);
+    {
+        if((c[i]=='a') || (c[i]=='e') || c[i]=='i' || c[i]=='o' || c[i]=='u')));
+        else
+        {
+          c1[j++]=c[i];
+        }
+    }
+    printf(c1); 
+}
---
+++ debug3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main() {
+  int n, r = 0, remainder;
+  scanf("%d", &n);
+    n=0
+  while (n != 0);{
+    remainder = n / 10;
+    reverse = reverse * 10 + remainder;
+    n /= 10;
+  }
+  printf(reverse);
+  return 0;
+}
